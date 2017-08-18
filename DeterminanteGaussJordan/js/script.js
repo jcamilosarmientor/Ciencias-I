c94faa2fe06e6fbe75c92eb5d2fdf4f0a67e317f
jcamilosarmientor <jcamilosarmientor@gmail.com>
2017-08-17 19:46:01 -0500

    Agregado boostrap

--- DeterminanteGaussJordan/js/script.js
+++ DeterminanteGaussJordan/js/script.js
@@ -1,25 +1,29 @@
 var numeros = []; // Números de la matriz
 
+
+$(document).ready(function() {
+  $('#btnCalcular').hide()
+});
+
 function agregarCajas() {
   var numCajas = $('#inputCantidad').val();
   var divMatriz = $('#matriz');
-  var caja = ''; // Caja a crear
-
   // Valida si ya existe el input_0, para eliminar los input ya existentes y volver a crear unos nuevos
   // Esto es en caso de que el valor de numCajas cambie
   if (typeof $('#input_0').value === 'undefined') {
     divMatriz.empty();
   }
   // Agrega los input al div 'cajas' y les asigna un id contenenando 'input_' con el valor de i
-  for (var i = 0; i < numCajas/2; i++) {
-    for (var j = 0; j < numCajas/2; j++) {
-      var inputCaja = document.createElement('input');
-      caja = '<input type="number" id="input_'+i+'_'+j+'">';
-      divMatriz.append(caja);
+  for (var i = 0; i < numCajas; i++) {
+    var divFila = '<div id="fila_'+i+'" class="row"></div>';
+    divMatriz.append(divFila);
+    for (var j = 0; j < numCajas; j++) {
+      var newInput = '<input type="number" class="form-control col-md-'+Math.trunc(12/numCajas)+' col-md-offset-1" id="input_'+i+'_'+j+'">';
+      $('#fila_'+i).append(newInput);
     }
-    divMatriz.append('<br>');
   }
   $('#btnCalcular').show();
+  $('#txtTamanioMatriz').html('Matriz de tamaño '+numCajas+'x'+numCajas);
 }
 
 function llenarMatriz() {
@@ -45,13 +49,13 @@ function calcularDeterminante () {
       // Si toda la fila es 0, el determinante no existe
       // Si se intercambia con una fila de abajo se multiplica el determinante por -1
     }
-    for (var j = i+1; j < n; j++) {
-      for (var k = j+1; k < n; k++) {
-        console.log(typeof m);
-        m[j][k] = m[j][k]-((m[k][i]*m[i][j])/m[i][j]);
+    for (var k = i+1; k < n; k++) {
+      for (var j = i+1; j < n; j++) {
+        m[k][j] = m[k][j] - (m[k][j]*m[i][j])/m[i][i];
       }
     }
   }
+  console.log(m[0][0]);
   var d = 1;  // determinante
   for (var i = 0; i < n; i++) {
     d = d * m[i][i];
