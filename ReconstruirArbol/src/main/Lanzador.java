1ada9ee63b05ff08f131e48d32205a7124d00f00
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2017-11-13 22:42:35 -0500

    Reconstruir árbol
    Completado el ejercicio de reconstruir árbol a partir de sus recorridos en inorden con preorden o posorden

---
+++ ReconstruirArbol/src/main/Lanzador.java
@@ -0,0 +1,17 @@
+package main;
+
+import cliente.Principal;
+
+/**
+ *
+ * @author jcamilosarmientor
+ */
+public class Lanzador {
+
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        
+    }
+    
+}
