6568680b4c95ac586261092ada0bcbc55da14ac5
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2017-11-14 01:52:26 -0500

    Arbol rojinegro
    Finalizado el ejercicio de arboles rojinegro

---
+++ ArbolRojoNegro/src/main/Principal.java
@@ -0,0 +1,13 @@
+package main;
+
+import cliente.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Window w = new Window();
+		w.setVisible(true);
+	}
+
+}
